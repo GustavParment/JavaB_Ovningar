1fba67d5fdd73255bac0793fb174718fc3d5449d
GustavParment <gustav.parment1993@gmail.com>
2023-10-13 19:03:39 +0200

    first commit

--- src/com/Gustav/demo/Main.java
+++ src/com/Gustav/demo/Main.java
@@ -1,41 +1,12 @@
 package com.Gustav.demo;
 
 
-import java.util.ArrayList;
 
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
-
-        List<Student> students = new ArrayList<>();
-
-        Student benny = new Student("Benny", 15,true);
-        Student frida = new Student("Frida", 22,false);
-
-
-
-
-
-        students.add(benny);
-        students.add(frida);
-        students.sort(Collections.reverseOrder());
-        System.out.println(students);
-
-        System.out.println(benny.getAge() + " " +  benny.getName());
-        System.out.println(frida.getAge() + " " + frida.getName());
-
-
-
-        System.out.println("List before swapping: " + students );
-
-        Collections.swap(students, 0 ,1);
-
-        System.out.println("List after swapping: " + students); // Utan att implementera Student class
 
 
 
