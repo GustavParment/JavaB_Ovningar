7fef1f8817a3429759ab03560df3b623ea5875f4
GustavParment <gustav.parment1993@gmail.com>
2023-10-13 15:28:56 +0200

    final commit

--- src/com/Gustav/demo/Main.java
+++ src/com/Gustav/demo/Main.java
@@ -8,6 +8,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(myNumberListReversed());
+
+
+    }
+
+    public static ArrayList myNumberListReversed(){
+
+        List<Integer> numberList = new ArrayList<>();
+
+        numberList.add(1);
+        numberList.add(2);
+        numberList.add(3);
+        numberList.add(4);
+        numberList.add(5);
+        numberList.add(6);
+        numberList.add(7);
+        numberList.add(8);
+        numberList.add(9);
+        numberList.add(10);
+        Collections.sort(numberList, Collections.reverseOrder());
+
+        return (ArrayList) numberList;
     }
 }
 
